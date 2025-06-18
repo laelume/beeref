318553433f90d829484e6087f7c0efdca705f94e
ashlae blum'e <laelume@gmail.com>
2025-06-18 16:46:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     entry_points={
-        'console_scripts': [
-            'beeref=beeref.main:main',
-            'beeref-cli=beeref.cli:cli_main',
-        ],
-    }
+    'gui_scripts': [
+        'beeref=beeref.__main__:main',  # existing
+    ],
+    'console_scripts': [
+        'beeref-cli=beeref.cli:cli_main',  # NEW LINE
+    ],
+}
 )
